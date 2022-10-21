cb9a4263b6c83df0c86c5129e07b002f970b0999
DevAgani <dev.things.ke@gmail.com>
2022-10-21 07:26:22 +0200

    Make `HTTPClient` a protocol create a SPY to check the requests made

--- LaterGramTests/TestCases/LaterGramImageLoaderTests.swift
+++ LaterGramTests/TestCases/LaterGramImageLoaderTests.swift
@@ -8,19 +8,45 @@
 import XCTest
 
 class LaterGramImageLoader {
+    private var client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "http://dummy-url.com")!)
+    }
 }
-
-class HTTPClient {
-    var requestedURL: URL?
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
 final class LaterGramImageLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestData(){
-        let client = HTTPClient()
-        _ = LaterGramImageLoader()
+        let client = HTTPClientSpy()
+        _ = LaterGramImageLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = LaterGramImageLoader(client: client)
+        
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
+    }
+    
+    // MARK: - Helpers
+    private class HTTPClientSpy: HTTPClient {
+        
+        var requestedURL: URL?
+       
+        func get(from url: URL) {
+            self.requestedURL = url
+        }
+        
     }
 }
