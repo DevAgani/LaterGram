2b191a78aa4e39c7e63bf1b35a703a4b9ca5bdff
DevAgani <dev.things.ke@gmail.com>
2022-10-21 14:56:43 +0200

    Improve the extension to include a helper that'll will add query items

--- LaterGram/Extensions/URL+Sign.swift
+++ LaterGram/Extensions/URL+Sign.swift
@@ -13,8 +13,20 @@ extension URL {
     /// - Parameter token: valid token
     /// - Returns: URL
     public func signed(with token: String) -> URL {
+        let url = self
+        url.addQueryItems(ofKey: "access_token", token)
+        return url
+    }
+    
+    /// Adds query items to a url
+    /// - Parameters:
+    ///   - key: key
+    ///   - item: values
+    /// - Returns: `URL`
+    @discardableResult
+    public func addQueryItems(ofKey key: String, _ item: String) -> URL {
         var url = self
-        let tokenQueryItem = URLQueryItem(name: "access_token", value: token)
+        let tokenQueryItem = URLQueryItem(name: key, value: item)
         url.append(queryItems: [tokenQueryItem])
         return url
     }
