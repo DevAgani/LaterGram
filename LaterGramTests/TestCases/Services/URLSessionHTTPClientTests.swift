83d808fe0143528bc1ef359f44df7a45efabfc63
DevAgani <dev.things.ke@gmail.com>
2022-10-21 10:52:14 +0200

    Add a `URLProtocolStub` in preparation of writing `URLSessionHTTPClient` as an implementation of `HTTPClient`

---
+++ LaterGramTests/TestCases/Services/URLSessionHTTPClientTests.swift
@@ -0,0 +1,74 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  LaterGramTests
+//
+//  Created by George Nyakundi on 21/10/2022.
+//
+
+import XCTest
+
+final class URLSessionHTTPClientTests: XCTestCase {
+    
+    // MARK: - Helpers
+    
+    /// Intercept URLSessionHTTPClient requests and stub the responses
+    private class URLProtocolStub: URLProtocol {
+        private static var stub: Stub?
+        private static var requestObserver: ((URLRequest) -> Void)?
+        
+        private struct Stub {
+            let data: Data?
+            let response: URLResponse?
+            let error: Error?
+        }
+        
+        static func stub(data: Data?, response: URLResponse?, error: Error? = nil ) {
+            stub = Stub(data: data, response: response, error: error)
+        }
+        
+        static func startInterceptingRequests() {
+            URLProtocolStub.registerClass(URLProtocolStub.self)
+        }
+        
+        static func stopInterceptionRequests() {
+            URLProtocolStub.unregisterClass(URLProtocolStub.self)
+            stub = nil
+            requestObserver = nil
+        }
+        
+        override class func canInit(with request: URLRequest) -> Bool {
+            return true
+        }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+        
+        static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+            requestObserver = observer
+        }
+        
+        override func startLoading() {
+            
+            if let requestObserver = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return requestObserver(request)
+            }
+            
+            if let data = URLProtocolStub.stub?.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = URLProtocolStub.stub?.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            
+            if let error = URLProtocolStub.stub?.error {
+                client?.urlProtocol(self, didFailWithError: error)
+            }
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        override func stopLoading() {}
+    }
+}
