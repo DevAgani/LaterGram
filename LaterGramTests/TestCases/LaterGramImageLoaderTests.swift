6a6109d041b168eb3f8b5c8be08d61cedd5a38b8
DevAgani <dev.things.ke@gmail.com>
2022-10-21 07:35:32 +0200

    Given a url `LaterGramImageLoader` requests from this url

--- LaterGramTests/TestCases/LaterGramImageLoaderTests.swift
+++ LaterGramTests/TestCases/LaterGramImageLoaderTests.swift
@@ -8,14 +8,16 @@
 import XCTest
 
 class LaterGramImageLoader {
-    private var client: HTTPClient
+    let url: URL
+    let client: HTTPClient
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     func load() {
-        client.get(from: URL(string: "http://dummy-url.com")!)
+        client.get(from: url)
     }
 }
 protocol HTTPClient {
@@ -26,19 +28,20 @@ final class LaterGramImageLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestData(){
         let client = HTTPClientSpy()
-        _ = LaterGramImageLoader(client: client)
+        _ = LaterGramImageLoader(url: url(), client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestsDataFromURL() {
+    func test_load_requestsDataFromAGivenURL() {
+        let givenURL = url("https://very-secure-url.com")
         let client = HTTPClientSpy()
-        let sut = LaterGramImageLoader(client: client)
+        let sut = LaterGramImageLoader(url: givenURL, client: client)
         
         sut.load()
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, givenURL)
     }
-    
+  
     // MARK: - Helpers
     private class HTTPClientSpy: HTTPClient {
         
@@ -47,6 +50,9 @@ final class LaterGramImageLoaderTests: XCTestCase {
         func get(from url: URL) {
             self.requestedURL = url
         }
-        
+    }
+    
+    private func url(_ string: String = "https://dummy-url.com") -> URL {
+        URL(string: string)!
     }
 }
